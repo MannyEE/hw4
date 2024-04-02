b27d0858260e1f4e4bc64b3eaf99aa7b8d4ebb7c
Emmanuel Ezirim <Eezirim23@gmail.com>
2024-04-01 20:20:02 -0700

    semi final

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -4,15 +4,40 @@
 #endif
 
 #include "equal-paths.h"
+#include <algorithm>
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int treeDepth(Node* root, bool& validTree);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
 
+    bool validTree = true;
+    treeDepth(root, validTree);
+ 
+    return validTree;
+ 
 }
+
+int treeDepth(Node* root, bool& validTree){
+
+    if (root == nullptr)return 0;
+    if (validTree == false) return 0;
+
+    int leftHeight = treeDepth(root->left, validTree);
+    int rightHeight = treeDepth(root->right, validTree);
+
+    if (((root->left != nullptr) && (root->right != nullptr)) && (leftHeight != rightHeight)) validTree = false; 
+    else if ( (root->left == nullptr) && (root->right != nullptr)) return (1 + rightHeight);
+    else if ( (root->left != nullptr) && (root->right == nullptr)) return (1 + leftHeight);
+    
+    if (leftHeight > rightHeight) return (1 + leftHeight);
+    else return (1 + rightHeight);
+
+
+}
+
 
